3a2ff97938003e70cfa8de9bd8a6632831117440
gamiz <71780447+satankaos@users.noreply.github.com>
2022-11-23 19:32:30 +0100

    meet 1

---
+++ 01ModulosCore/00-basics.js
@@ -0,0 +1,14 @@
+/*
+Esto es un primer ejemplo de un archivo
+ejecutado desde node
+*/
+DAW="wai"
+console.log('Hola Mundo desde Node.js, esto se verá en el terminal de comandos')
+console.log(2 + 5)
+console.log(DAW)
+setInterval(function (){
+   console.log('Hola Node.js')
+}, 1000)
+//Ctrl + C detengo la ejecución de un script en la consola
+
+

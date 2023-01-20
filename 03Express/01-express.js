d41c5c213b326c2c01d6d6205327c592e5afb4e1
gamiz <71780447+satankaos@users.noreply.github.com>
2023-01-20 10:31:10 +0100

    Changes to be committed: #	modified:   03Express/01-express.js #

--- 03Express/01-express.js
+++ 03Express/01-express.js
@@ -32,10 +32,10 @@ app.use(express.static(__dirname + '/public'))
 app.set('view engine', 'ejs')
 app.set('views', __dirname + '/views')
 app.use('/', require('./router/rutas'))
-//app.use('/pokemon', require('./router/pokemon'))
+app.use('/pokemon', require('./router/pokemon'))
 //Entrenador
 //SII DESCOMENTO ESTO NO ME VA 
-app.use('/entrenador',require('./router/entrenador'))
+//app.use('/entrenador',require('./router/entrenador'))
 
 
 //Listen port
